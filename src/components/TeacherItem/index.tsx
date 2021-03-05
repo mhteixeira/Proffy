c3935f35b27362025b406c1ef1e0280b122cfe8b
Murillo Teixeira <murillo.teixeira@usp.br>
2021-03-05 19:14:17 -0300

    Commit everything
    eu n sabia que tinha q entregar no git mals

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import './styles.css';
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg"
+import api from '../../services/api';
+
+export interface Teacher {
+    id: number;
+    cost: number;
+    subject: string;
+    user: {
+        avatar: string;
+        bio: string;
+        name: string;
+        whatsapp: string;
+    }
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    };
+
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src={teacher.user.avatar} alt={teacher.user.name}/>
+                <div>
+                    <strong>{teacher.user.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+            <p>{teacher.user.bio}</p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.user.whatsapp}`}>
+                    <img src={whatsappIcon} alt="WhatsApp"/>
+                    Entrar em contato
+                </a>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
